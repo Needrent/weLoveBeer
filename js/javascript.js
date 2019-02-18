b4cdb2fbf4bc5f8ec7b76d4ce63275833eef12c5
SEJ <sej@needrent.dk>
2019-02-18 10:34:10 +0100

    Initiate document

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+const link = "https://spreadsheets.google.com/feeds/list/10k5FHg8AolohKoybFTJarvWLSzhzOe-jr8HbHa6KZwU/od6/public/values?alt=json";
+
+const section = document.querySelector("section");
+const template = document.querySelector("#monsterMash").content;
+
+
+function load(link){
+    fetch(link).then(e=>e.json()).then(data=>data.feed.entry.forEach(displayList));
+}
+    console.log(section);
+function displayList(data){
+    const clone = template.cloneNode("true");
+    clone.querySelector("h2").textContent = data.gsx$name.$t;
+
+    section.appendChild(clone);
+}
+load(link);

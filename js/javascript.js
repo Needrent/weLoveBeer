4e33058e1c5a07ecca7518c35ea950fa831630a2
SEJ <sej@needrent.dk>
2019-02-21 14:22:52 +0100

    Finished the JS + Styling on home

--- js/javascript.js
+++ js/javascript.js
@@ -7,10 +7,30 @@ const template = document.querySelector("template").content;
 function load(link){
     fetch(link).then(e=>e.json()).then(data=>data.feed.entry.forEach(displayList));
 }
-    console.log(section);
+
 function displayList(data){
     const clone = template.cloneNode("true");
     clone.querySelector("h2").textContent = data.gsx$name.$t;
+    clone.querySelector(".type").textContent = data.gsx$type.$t;
+    clone.querySelector(".country").textContent = data.gsx$country.$t;
+    clone.querySelector(".brewery").textContent = data.gsx$brewery.$t;
+    clone.querySelector(".alcohol").textContent = "VOL " + data.gsx$alcohol.$t + " %";
+    clone.querySelector(".author a").textContent = data.gsx$author.$t;
+    clone.querySelector("p").textContent = data.gsx$shortdescription.$t;
+
+    if(data.gsx$season.$t == "1"){
+        clone.querySelector(".season").textContent = "Seasonal";
+    }
+
+
+    if(data.gsx$image.$t == ""){
+        clone.querySelector("img").src = "/img/placeholder.png";
+    }
+    else{
+        clone.querySelector("img").src ="/img/" + data.gsx$image.$t;
+        clone.querySelector("img").alt = data.gsx$name.$t;
+    }
+
 
     section.appendChild(clone);
 }
